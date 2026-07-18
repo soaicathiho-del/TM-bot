cf0e1f677bf5d263f127a36250aceee91ba131e8
soaicathiho-del <soaicathiho@gmail.com>
2026-07-18 11:38:34 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,20 +2,11 @@ import os
 
 
 class Config:
-    """
-    Đọc toàn bộ cấu hình từ GitHub Secrets
-    """
-
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     CHAT_ID = os.getenv("CHAT_ID")
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-    # Model Gemini (chỉ sửa dòng này nếu Google đổi model)
+    # Model Gemini
     MODEL = "gemini-3.5-flash"
 
-    # Telegram
     TELEGRAM_API = "https://api.telegram.org"
-
-    # Đường dẫn dữ liệu
-    PROMPT_FILE = "prompt.txt"
-    HISTORY_FILE = "data/history.json"
