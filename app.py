b1147da217f1bc9ab4a7133249643a126e4df6aa
soaicathiho-del <soaicathiho@gmail.com>
2026-07-19 19:27:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from config import Config
+from notion_service import get_today_tasks
 
-print(Config.NOTION_TOKEN)
-print(Config.TM_DAILY_DATABASE_ID)
+tasks = get_today_tasks()
+
+print(f"Today's Tasks: {len(tasks)}")
+
+for task in tasks:
+    props = task["properties"]
+
+    title = props["Task"]["title"][0]["plain_text"]
+    task_type = props["Type"]["select"]["name"]
+
+    print(f"{title} | {task_type}")
