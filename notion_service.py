065e5f5a99013ccd160174d61f73fcac58d38096
soaicathiho-del <soaicathiho@gmail.com>
2026-07-19 19:21:37 +0700

    Update notion_service.py

--- notion_service.py
+++ notion_service.py
@@ -1,15 +1,23 @@
+from datetime import datetime
+
 from notion_client import Client
 
 from config import Config
-
 
 notion = Client(auth=Config.NOTION_TOKEN)
 
 
 def get_today_tasks():
+    today = datetime.today().strftime("%Y-%m-%d")
 
     response = notion.databases.query(
-        database_id=Config.TM_DAILY_DATABASE_ID
+        database_id=Config.TM_DAILY_DATABASE_ID,
+        filter={
+            "property": "Ngày",
+            "date": {
+                "equals": today
+            }
+        }
     )
 
     return response["results"]
