9379ef81ee1d0cb14b772d5124f310ff9ccfb6cd
soaicathiho-del <soaicathiho@gmail.com>
2026-07-19 19:18:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
-from notion_service import get_today_tasks
+from config import Config
 
-tasks = get_today_tasks()
-
-print(f"Found {len(tasks)} tasks")
-
-for task in tasks:
-    try:
-        title = task["properties"]["Task"]["title"][0]["plain_text"]
-    except:
-        title = "No title"
-
-    print(title)
+print(Config.NOTION_TOKEN)
+print(Config.TM_DAILY_DATABASE_ID)
