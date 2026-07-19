72b9795f0076a13a1fd97847eaac2a997f83bee4
soaicathiho-del <soaicathiho@gmail.com>
2026-07-19 19:14:57 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,10 @@ class Config:
     CHAT_ID = os.getenv("CHAT_ID")
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-    # Model Gemini
+    NOTION_TOKEN = os.getenv("NOTION_TOKEN")
+    TM_DAILY_DATABASE_ID = os.getenv("TM_DAILY_DATABASE_ID")
+    RULES_POINT_DATABASE_ID = os.getenv("RULES_POINT_DATABASE_ID")
+
     MODEL = "gemini-3.5-flash"
 
     TELEGRAM_API = "https://api.telegram.org"
-
-NOTION_TOKEN = os.getenv("NOTION_TOKEN")
-
-TM_DAILY_DATABASE_ID = os.getenv("TM_DAILY_DATABASE_ID")
-
-RULES_POINT_DATABASE_ID = os.getenv("RULES_POINT_DATABASE_ID")
