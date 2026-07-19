b294a9c0200638d84c3e46e878471cac3b4819db
soaicathiho-del <soaicathiho@gmail.com>
2026-07-19 17:36:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,38 @@
-from gemini_service import test_gemini
+from config import Config
+from gemini_service import ask_gemini
 from telegram_service import send_message
 
 
-def main():
-    """
-    Chạy bot.
-    """
-
-    message = "🤖 Gemini OK!\n\n"
-    message += test_gemini()
-
-    send_message(message)
+def load_file(file_name):
+    try:
+        with open(file_name, "r", encoding="utf-8") as f:
+            return f.read()
+    except FileNotFoundError:
+        return ""
 
 
-if __name__ == "__main__":
-    main()
+system_prompt = load_file("system_prompt.md")
+user_profile = load_file("user_profile.md")
+daily_prompt = load_file("prompt.txt")
+
+prompt = f"""
+{system_prompt}
+
+=========================
+USER PROFILE
+=========================
+
+{user_profile}
+
+=========================
+TODAY TASK
+=========================
+
+{daily_prompt}
+"""
+
+response = ask_gemini(prompt)
+
+send_message(response)
+
+print("Done.")
