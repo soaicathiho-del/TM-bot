d546117b3315d67e4669929c439b0ab82ac590c3
soaicathiho-del <soaicathiho@gmail.com>
2026-07-19 17:33:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,17 @@
-name: TM Daily
+from gemini_service import test_gemini
+from telegram_service import send_message
 
-on:
-  workflow_dispatch:
 
-  schedule:
-    # Sáng
-    - cron: "40 7 * * *"
+def main():
+    """
+    Chạy bot.
+    """
 
-    # Chiều
-    - cron: "00 19 * * *"
+    message = "🤖 Gemini OK!\n\n"
+    message += test_gemini()
 
-    # Tối
-    - cron: "00 3 * * *"
+    send_message(message)
 
-jobs:
-  send-message:
-    runs-on: ubuntu-latest
 
-    steps:
-      - name: Checkout repository
-        uses: actions/checkout@v4
-
-      - name: Setup Python
-        uses: actions/setup-python@v5
-        with:
-          python-version: "3.12"
-
-      - name: Install dependencies
-        run: |
-          python -m pip install --upgrade pip
-          pip install -r requirements.txt
-
-      - name: Run application
-        env:
-          BOT_TOKEN: ${{ secrets.BOT_TOKEN }}
-          CHAT_ID: ${{ secrets.CHAT_ID }}
-          GEMINI_API_KEY: ${{ secrets.GEMINI_API_KEY }}
-        run: python app.py
+if __name__ == "__main__":
+    main()
