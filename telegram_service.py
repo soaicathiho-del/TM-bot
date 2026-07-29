e5f42dda71f396e169d6a492f2a22f4bf62a22d8
soaicathiho-del <soaicathiho@gmail.com>
2026-07-29 10:16:23 +0700

    Update telegram_service.py

--- telegram_service.py
+++ telegram_service.py
@@ -1,12 +1,34 @@
 import requests
-from config import BOT_TOKEN
+
+from config import Config
+
 
 class TelegramService:
-    def __init__(self):
-        self.base_url = f"https://api.telegram.org/bot{BOT_TOKEN}"
 
-    def send_message(self, chat_id, text):
+    def __init__(self):
+
+        self.base_url = (
+            f"https://api.telegram.org/bot{Config.BOT_TOKEN}"
+        )
+
+    def send_message(
+        self,
+        chat_id,
+        text,
+    ):
+
         url = f"{self.base_url}/sendMessage"
-        data = {"chat_id": chat_id, "text": text, "parse_mode": "Markdown"}
-        response = requests.post(url, json=data)
+
+        data = {
+            "chat_id": chat_id,
+            "text": text,
+            "parse_mode": "Markdown",
+        }
+
+        response = requests.post(
+            url,
+            json=data,
+            timeout=30,
+        )
+
         return response.json()
